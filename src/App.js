236a26434e6c760cef998ed3ee9c3e708e5edda7
Ibrokhimweb <ibrokhimgithub@gmail.com>
2022-09-01 11:44:23 +0500

    kamchiliklar to'g'irlandi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+// Import React
+import { React } from 'react'
+
+// Import React-Router-Dom
+import { BrowserRouter } from "react-router-dom";
+
 // Import Css
 import './App.css';
 
+// Import Components
+import Accessories from './Components/Accessories/Accessories'
+
+
 function App() {
-  return (
-    <>
-      <h1>App.js</h1>
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <Accessories />
+        </BrowserRouter>
+    );
 }
 
 export default App;
